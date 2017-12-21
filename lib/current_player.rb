a8a7675e08de2b3b93bb3e58f9044d87e368fa4a
Elli Scharlin <escharlin@wesleyan.edu>
2017-12-21 18:35:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,7 +11,7 @@ end
 
 def current_player(board)
   count = turn_count(board)
-  turn = count%2==1 ? "X" : "O"
+  turn = count%2==0 ? "X" : "O"
   #Ternary where if it is o's turn it is false
 
 end
